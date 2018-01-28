30c5e474c76a65e5b407e440892a1f660c6d3ae3
Daniel Berrocal <jdanielb1992@gmail.com>
2018-01-28 16:39:40 -0600

    adding route validations

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel() {
+    let isAuthenticated = localStorage.getItem('ember-test-app-is-user-authenticated');
+    isAuthenticated = isAuthenticated === 'true';
+    if (!isAuthenticated) {
+      this.transitionTo('/auth');
+    }
+  },
 });
